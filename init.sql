b68a07c70dec8a6ac12c07719a094cb808c4f674
IanLemee <ianlemee@hotmail>
2026-05-04 11:07:42 -0300

    adding init.sql

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE SEQUENCE IF NOT EXISTS referenceSeqGen
+START WITH 1
+INCREMENT 1;
+
+CREATE TABLE IF NOT EXISTS reference_data(
+  id BIGINT PRIMARY KEY DEFAULT nextval('referenceSeqGen'),
+    vectors vector(14),
+    is_fraud boolean
+);
